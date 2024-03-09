6c0dfc3452113dc2631acb0c476f2398e8368a2f
Abhishek <abhishek.pub9503@gmail.com>
2024-03-09 23:28:08 +0530

    some changes

--- build/clients/db/index.js
+++ build/clients/db/index.js
@@ -1,5 +1,12 @@
 "use strict";
+// import { PrismaClient } from "@prisma/client";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prismaClient = void 0;
+// export const prismaClient  = new PrismaClient({log :["query"]})
 const client_1 = require("@prisma/client");
-exports.prismaClient = new client_1.PrismaClient({ log: ["query"] });
+// export const prismaClient = new PrismaClient({
+//   log: ["query"], // Enable query logging
+// });
+exports.prismaClient = new client_1.PrismaClient({
+    log: ["query", "info", "warn", "error"],
+});
--- src/clients/db/index.ts
+++ src/clients/db/index.ts
@@ -1,4 +1,16 @@
+// import { PrismaClient } from "@prisma/client";
+
+
+// export const prismaClient  = new PrismaClient({log :["query"]})
+
+
 import { PrismaClient } from "@prisma/client";
 
+// export const prismaClient = new PrismaClient({
+//   log: ["query"], // Enable query logging
+// });
 
-export const prismaClient  = new PrismaClient({log :["query"]})
+
+export const prismaClient = new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
